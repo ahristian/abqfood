6627df932b15f9673cb5fa186442cf0a83b6f64f
ahristian <hristian.andrei@gmail.com>
2020-10-06 11:55:56 -0600

    added footer

--- app/public/wp-content/themes/foodAbq/footer.php
+++ app/public/wp-content/themes/foodAbq/footer.php
@@ -7,7 +7,7 @@
         <div class="group">
             <div class="site-footer__col-one">
                 <h1 class="school-logo-text school-logo-text--alt-color">
-                    <a href="#"><strong>Albuquerque&rsquo;s</strong> Cuisine</a>
+                    <a href="<?php echo site_url()?>">Taste of <strong>New Mexico</strong></a>
                 </h1>
                 <p><a class="site-footer__link" href="#">555.555.5555</a></p>
             </div>
@@ -17,7 +17,7 @@
                     <h3 class="headline headline--small">Explore</h3>
                     <nav class="nav-list">
                         <ul>
-                            <li><a href="#">About Us</a></li>
+                            <li><a href="<?php echo site_url('/about-us')?>">About Us</a></li>
                             <li><a href="#">Best Places to eat</a></li>
                             <li><a href="#">Events</a></li>
                             <li><a href="#">Blog</a></li>
@@ -30,7 +30,7 @@
                     <nav class="nav-list">
                         <ul>
                             <li><a href="#">Legal</a></li>
-                            <li><a href="#">Privacy</a></li>
+                            <li><a href="<?php echo site_url('privacy-policy')?>">Privacy</a></li>
                             <li><a href="#">Careers</a></li>
                         </ul>
                     </nav>
