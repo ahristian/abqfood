0aa4690d6c7c28cf015a04029a925d72eb65b0af
ahristian <hristian.andrei@gmail.com>
2020-10-08 12:16:26 -0600

    added tyle

--- app/public/wp-content/themes/foodAbq/functions.php
+++ app/public/wp-content/themes/foodAbq/functions.php
@@ -1,9 +1,16 @@
 <?php
 function foodAbq () {
-    wp_enqueue_script('javaScript', get_theme_file_uri('/js/scripts-bundled.js'), NULL, '1.0', true);
     wp_enqueue_style('google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
     wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
-    wp_enqueue_style('foodAbq', get_stylesheet_uri());
+   if (strstr($_SERVER['SERVER_NAME'], 'foodabq.local')) {
+       wp_enqueue_script('javaScript', 'http://localhost:3000/bundled.js', NULL, '1.0', true);
+
+   } else {
+       wp_enqueue_script('our-vendors-js', get_theme_file_uri('/bundled-assets/vendors~scripts.8c97d901916ad616a264.js'), NULL, '1.0', true);
+       wp_enqueue_script('javaScript', get_theme_file_uri('/bundled-assets/scripts.944507dd80df366d0788.js'), NULL, '1.0', true);
+       wp_enqueue_style('our-main-style', get_theme_file_uri('/bundled-assets/styles.944507dd80df366d0788.css'));
+   }
+
 }
 add_action('wp_enqueue_scripts', 'foodAbq');
 
