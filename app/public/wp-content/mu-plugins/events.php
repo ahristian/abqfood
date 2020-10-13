026ce271853ea90234742f59c2762b9ee3621159
ahristian <hristian.andrei@gmail.com>
2020-10-12 20:58:53 -0600

    added events

--- app/public/wp-content/mu-plugins/events.php
+++ app/public/wp-content/mu-plugins/events.php
@@ -1,17 +1,17 @@
 <?php
 
-function foodAbq_post_event () {
-    register_post_type('event', array(
+function foodAbq_post_types () {
+    register_post_type('events', array(
+        'has_archive' => true,
         'public' => true,
-        'menu_icon' => 'dashicons-buddicons-groups',
         'labels' => array(
             'name'=> 'Events',
             'add_new_item'=> 'Add new Event',
             'edit_item' => 'Edit Event',
             'all_items' => 'All Events',
             'singular_name' => 'Event'
-        )
+        ),
+        'menu_icon' => 'dashicons-buddicons-groups'
     ));
 }
-add_action('init', 'foodAbq_post_event');
-
+add_action('init', 'foodAbq_post_types');
