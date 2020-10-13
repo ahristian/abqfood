43c6c43d4d352d56781a06cdf3f7d024e57c3517
ahristian <hristian.andrei@gmail.com>
2020-10-12 21:02:08 -0600

    added events

--- app/public/wp-content/themes/foodAbq/front-page.php
+++ app/public/wp-content/themes/foodAbq/front-page.php
@@ -31,32 +31,42 @@ get_header();
     <div class="full-width-split__one">
         <div class="full-width-split__inner">
             <h2 class="headline headline--small-plus t-center">Upcoming Events</h2>
-
+            <?php
+            $homePageEvents = new WP_Query(array(
+                    'posts_per_page' => 5,
+                    'post_type' => 'events'
+                    )
+            );
+            while ($homePageEvents -> have_posts()) {
+                $homePageEvents ->the_post();
+                ?>
             <div class="event-summary">
-                <a class="event-summary__date t-center" href="#">
-                    <span class="event-summary__month">Mar</span>
-                    <span class="event-summary__day">25</span>
+                <a class="event-summary__date t-center" href=" <?php the_permalink(); ?>">
+                    <span class="event-summary__month">
+                         <?php the_time('M') ?>
+                    </span>
+                    <span class="event-summary__day">
+                         <?php the_time('d') ?>
+                    </span>
                 </a>
                 <div class="event-summary__content">
-                    <h5 class="event-summary__title headline headline--tiny"><a href="#">Poetry in the 100</a></h5>
-                    <p>Bring poems you&rsquo;ve wrote to the 100 building this Tuesday for an open mic and snacks. <a
-                                href="#" class="nu gray">Learn more</a></p>
+                    <h5 class="event-summary__title headline headline--tiny">
+                        <a href="<?php the_permalink(); ?>">
+                            <?php echo the_title(); ?>
+                        </a>
+                    </h5>
+                    <p> <?php echo wp_trim_words(get_the_content(), 18)?>
+                        <a href=" <?php the_permalink(); ?>" class="nu gray">
+                            Learn more</a>
+                    </p>
                 </div>
             </div>
-            <div class="event-summary">
-                <a class="event-summary__date t-center" href="#">
-                    <span class="event-summary__month">Apr</span>
-                    <span class="event-summary__day">02</span>
-                </a>
-                <div class="event-summary__content">
-                    <h5 class="event-summary__title headline headline--tiny"><a href="#">Quad Picnic Party</a></h5>
-                    <p>Live music, a taco truck and more can found in our third annual quad picnic day. <a href="#"
-                                                                                                           class="nu gray">Learn
-                            more</a></p>
-                </div>
-            </div>
+<?php
+            }
+            ?>
 
-            <p class="t-center no-margin"><a href="#" class="btn btn--blue">View All Events</a></p>
+            <p class="t-center no-margin">
+                <a href=" <?php echo site_url('events'); ?>" class="btn btn--blue">View All Events</a></p>
         </div>
     </div>
 
@@ -81,21 +91,22 @@ get_header();
                     </a>
                     <div class="event-summary__content">
                         <h5 class="event-summary__title headline headline--tiny">
-                            <a href="<?php the_permalink();?>">
+                            <a href="<?php the_permalink(); ?>">
                                 <?php the_title() ?>
                             </a>
                         </h5>
                         <p><?php echo wp_trim_words(get_the_content(), 18); ?>
-                            <a href="<?php the_permalink()?>;" class="nu gray">
+                            <a href="<?php the_permalink() ?>;" class="nu gray">
                                 Read more
                             </a>
                         </p>
                     </div>
                 </div>
                 <?php
-            } wp_reset_postdata();?>
+            }
+            wp_reset_postdata(); ?>
             <p class="t-center no-margin">
-                <a href="<?php echo site_url('/blog')?>;" class="btn btn--yellow">
+                <a href="<?php echo site_url('/blog') ?>;" class="btn btn--yellow">
                     View All Blog Posts</a></p>
         </div>
     </div>
