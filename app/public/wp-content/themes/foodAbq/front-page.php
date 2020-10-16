388b29bd20387bd6f5557b1141a495dbdbe7db90
ahristian <hristian.andrei@gmail.com>
2020-10-16 13:06:06 -0600

    added events

--- app/public/wp-content/themes/foodAbq/front-page.php
+++ app/public/wp-content/themes/foodAbq/front-page.php
@@ -35,33 +35,37 @@ get_header();
             $homePageEvents = new WP_Query(array(
                     'posts_per_page' => 5,
                     'post_type' => 'events'
-                    )
+                )
             );
-            while ($homePageEvents -> have_posts()) {
-                $homePageEvents ->the_post();
+            while ($homePageEvents->have_posts()) {
+                $homePageEvents->the_post();
                 ?>
-            <div class="event-summary">
-                <a class="event-summary__date t-center" href=" <?php the_permalink(); ?>">
+                <div class="event-summary">
+                    <a class="event-summary__date t-center" href=" <?php the_permalink(); ?>">
                     <span class="event-summary__month">
                          <?php the_time('M') ?>
                     </span>
-                    <span class="event-summary__day">
+                        <span class="event-summary__day">
                          <?php the_time('d') ?>
                     </span>
-                </a>
-                <div class="event-summary__content">
-                    <h5 class="event-summary__title headline headline--tiny">
-                        <a href="<?php the_permalink(); ?>">
-                            <?php echo the_title(); ?>
-                        </a>
-                    </h5>
-                    <p> <?php echo wp_trim_words(get_the_content(), 18)?>
-                        <a href=" <?php the_permalink(); ?>" class="nu gray">
-                            Learn more</a>
-                    </p>
+                    </a>
+                    <div class="event-summary__content">
+                        <h5 class="event-summary__title headline headline--tiny">
+                            <a href="<?php the_permalink(); ?>">
+                                <?php echo the_title(); ?>
+                            </a>
+                        </h5>
+                        <p> <?php if (has_excerpt()) {
+                                echo get_the_excerpt();
+                            } else {
+                                echo wp_trim_words(get_the_content(), 18);
+                            } ?>
+                            <a href=" <?php the_permalink(); ?>" class="nu gray">
+                                Learn more</a>
+                        </p>
+                    </div>
                 </div>
-            </div>
-<?php
+                <?php
             }
             ?>
 
@@ -95,7 +99,11 @@ get_header();
                                 <?php the_title() ?>
                             </a>
                         </h5>
-                        <p><?php echo wp_trim_words(get_the_content(), 18); ?>
+                        <p><?php if (has_excerpt()) {
+                               echo get_the_excerpt();
+                            } else {
+                                echo wp_trim_words(get_the_content(), 18);
+                            } ?>
                             <a href="<?php the_permalink() ?>;" class="nu gray">
                                 Read more
                             </a>
