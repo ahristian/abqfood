5fda7f64306b4563c8625af626451ee099c9d3f0
ahristian <hristian.andrei@gmail.com>
2020-10-16 13:04:27 -0600

    added events

--- app/public/wp-content/mu-plugins/events.php
+++ app/public/wp-content/mu-plugins/events.php
@@ -3,12 +3,14 @@
 function foodAbq_post_types () {
     register_post_type('events', array(
         'has_archive' => true,
+        'show_in_rest' => true,
+        'supports' => array('title', 'editor', 'excerpt'),
         'public' => true,
         'labels' => array(
             'name'=> 'Events',
             'add_new_item'=> 'Add new Event',
             'edit_item' => 'Edit Event',
-            'all_items' => 'All Events',
+            'all_items' => 'Events',
             'singular_name' => 'Event'
         ),
         'menu_icon' => 'dashicons-buddicons-groups'
