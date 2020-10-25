0bd08cb01402392793910d017c2a0381c2c5d5fc
ahristian <hristian.andrei@gmail.com>
2020-10-25 14:13:07 -0600

    added events history

--- app/public/wp-content/themes/foodAbq/functions.php
+++ app/public/wp-content/themes/foodAbq/functions.php
@@ -22,3 +22,25 @@ function foodAbq_features(){
 }
 
 add_action('after_setup_theme', 'foodAbq_features');
+
+function travelNewMexico_adjust_queries($query) {
+    if (!is_admin() AND
+        is_post_type_archive('events') AND
+        $query -> is_main_query()) {
+
+        $today = date('Ymd');
+        $query -> set('meta_key', 'event_date');
+        $query -> set('orderby', 'meta_value_num');
+        $query -> set('order', 'ASC');
+        $query -> set('meta_query', array(
+                            array(
+                                'key' => 'event_date',
+                                'compare' => '>=',
+                                'value' => $today,
+                                'type' => 'numeric'
+                            )
+                    )
+    );
+    }
+}
+add_action('pre_get_posts', 'travelNewMexico_adjust_queries');
