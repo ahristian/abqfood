fe32c9d3ba1e3f6f2e3cf691b59b6876ece6f677
ahristian <hristian.andrei@gmail.com>
2020-10-25 22:44:58 -0600

    added destinations

--- app/public/wp-content/mu-plugins/events.php
+++ app/public/wp-content/mu-plugins/events.php
@@ -1,10 +1,12 @@
 <?php
 
 function foodAbq_post_types () {
+    //Event Post Type
     register_post_type('events', array(
         'has_archive' => true,
         'show_in_rest' => true,
         'supports' => array('title', 'editor', 'excerpt', 'custom-fields'),
+        'rewrite' => array('slug' => 'events'),
         'public' => true,
         'labels' => array(
             'name'=> 'Events',
@@ -14,6 +16,23 @@ function foodAbq_post_types () {
             'singular_name' => 'Event'
         ),
         'menu_icon' => 'dashicons-buddicons-groups'
+    ));
+
+    //Destination Post Type
+    register_post_type('destination', array(
+        'has_archive' => true,
+        'show_in_rest' => true,
+        'supports' => array('title', 'editor', 'custom-fields'),
+        'rewrite' => array('slug' => 'destinations'),
+        'public' => true,
+        'labels' => array(
+            'name'=> 'Destinations',
+            'add_new_item'=> 'Add new Destinations',
+            'edit_item' => 'Edit Destination',
+            'all_items' => 'Destinations',
+            'singular_name' => 'Destination'
+        ),
+        'menu_icon' => 'dashicons-car'
     ));
 }
 
